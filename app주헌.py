70727282c112d885bc69d42da3f33dc96885d474
jjh714 <jeonjuheon1014@gmail.com>
2026-07-20 14:01:58 +0900

    Update app주헌.py

--- app주헌.py
+++ app주헌.py
@@ -1,7 +1,13 @@
 import streamlit as st
 
-st.title("카운터 앱")
-count = 0
-if st.button("증가"):
-    count = count+1
-st.markdown(f"## 현재 숫자: `{count}`")
+def reset_all():
+    st.session_state.user_name = ""
+    st.session_state.weather = "맑음"
+    st.session_state.top_type = "후드티"
+    st.session_state.top_color = "밝음"
+    st.session_state.bottom_type = "청바지"
+    st.session_state.bottom_color = "슬림"
+    st.session_state.shoes = "스니커즈"
+    st.session_state.acc = []
+
+st.button("전체 초기화", on_click=reset_all)
