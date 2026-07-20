85e0f7d01490761ce70220c097e9a87bb211e6c7
jjh714 <jeonjuheon1014@gmail.com>
2026-07-20 14:15:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,18 +24,18 @@ st.header("👕 아이템 조합하기")
 coll, col2 = st.columns(2)
 with coll:
     st.subheader("상의")
-    top_type = st.radio("종류", ["후드티", "셔츠", "맨투맨", "반팔 티셔츠"])
-    top_color = st.select_slider("색상 톤", options=["밝음", "무난함", "어두움"])
+    top_type = st.radio("종류", ["후드티", "셔츠", "맨투맨", "반팔 티셔츠"], key="top_type")
+    top_color = st.select_slider("색상 톤", options=["밝음", "무난함", "어두움"], key="top_color")
 with col2:
     st.subheader("하의")
-    bottom_type = st.radio("종류", ["청바지", "슬랙스", "트레이닝 팬츠", "반바지"])
-    bottom_color = st.select_slider("핏(Fit)", options=["슬림", "레귤러", "오버핏"])
+    bottom_type = st.radio("종류", ["청바지", "슬랙스", "트레이닝 팬츠", "반바지"], key="bottom_type")
+    bottom_color = st.select_slider("핏(Fit)", options=["슬림", "레귤러", "오버핏"], key="bottom")
 
 st.header("디테일 추가")
 tab1, tab2 = st.tabs(["신발", "액세서리"])
 with tab1:
     st.write("오늘의 발걸음을 책임질 신발:")
-    shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"])
+    shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"], key="shoes")
     with st.expander("신발 선택 팁 보기"):
         st.info("너무 튀는 신발은 지양하도록 해요!")
 with tab2:
