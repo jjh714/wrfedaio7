84006429489d6de24a4a75b284c10d5635a5a700
jjh714 <jeonjuheon1014@gmail.com>
2026-07-21 10:01:54 +0900

    Update ㄱㄷㅎㅁㄱㄷㅎㅁ.py

--- ㄱㄷㅎㅁㄱㄷㅎㅁ.py
+++ ㄱㄷㅎㅁㄱㄷㅎㅁ.py
@@ -1,16 +1,45 @@
 import streamlit as st
 
-st.set_page_config(page_title="공룡 버튼", page_icon="🦖")
+st.set_page_config(page_title="움직이는 공룡", page_icon="🦖")
 
-st.title("🦖 공룡 등장!")
-st.write("아래 버튼을 눌러보세요!")
+st.title("🦖 움직이는 공룡")
 
-if st.button("공룡 소환하기"):
+st.write("버튼을 누르면 공룡이 달립니다!")
+
+if st.button("공룡 출발!"):
     st.balloons()
-    st.markdown(
-        """
-        # 🦖
-        ## 안녕! 나는 공룡이야!
-        """
-    )
-    st.success("공룡이 나타났습니다!")
+
+    st.markdown("""
+    <style>
+    .ground {
+        position: relative;
+        width: 100%;
+        height: 150px;
+        border-bottom: 4px solid green;
+        overflow: hidden;
+    }
+
+    .dino {
+        position: absolute;
+        font-size: 70px;
+        left: -100px;
+        bottom: 10px;
+        animation: run 5s linear forwards;
+    }
+
+    @keyframes run {
+        from {
+            left: -100px;
+        }
+        to {
+            left: 100%;
+        }
+    }
+    </style>
+
+    <div class="ground">
+        <div class="dino">🦖</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.success("공룡이 달리고 있습니다! 🦖")
