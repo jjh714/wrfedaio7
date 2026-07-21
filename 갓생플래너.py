de87b13ea8b725074ef02aa458f0430aa928e80b
jjh714 <jeonjuheon1014@gmail.com>
2026-07-21 10:48:34 +0900

    Update 갓생플래너.py

--- 갓생플래너.py
+++ 갓생플래너.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from openai import OpenAI
 ai_client = OpenAI (api_key=st.secrets["OPENΑΙ_ΑΡΙ_KEY"])
+
 if 'todo_list' not in st.session_state:
     st.session_state.todo_list = []
 if 'user_motto' not in st.session_state:
@@ -76,13 +77,6 @@ def page_report():
             st.session_state.todo_list = []
             st.rerun()
 
-pg = st.navigation([
-    st.Page(page_motto, title="오늘의 다짐", icon="📣"),
-    st.Page(page_todo, title="오늘의 할 일", icon="✅"),
-    st.Page(page_report, title="나의 갓생 지수", icon="📈")], position="top")
-
-st.title("🌱 갓생 살기 플래너")
-pg.run()
 def page_ai_coach():
     st.header("🤖 AI 코치와 대화하기")
     if "messages" not in st.session_state:
@@ -92,12 +86,27 @@ def page_ai_coach():
     for message in st.session_state.messages:
         if message["role"] != "system":
             with st.chat message(message["role"]):
-                st.markdown (message ["content"])
-prompt = st.text_input("질문을 입력하세요")
-    if st.button("보내기"):
-        response = ai_client.responses.create(
-            model="gpt-5.4-mini",
-            input=prompt
-        )
-        st.write(response.output_text)
+             st.markdown (message ["content"])
+question = st.chat_input("질문을 입력하세요")
+    if question:
+        st.session_state.messages.append({"role": "user", "content": question})
+        with st.chat_message("user"):
+            st.markdown(question)
+        with st.chat_message("assistant"):
+            status_context = f"현재 나의 할 일과 달성 여부: {st.session_state.todo_list}"
+            prompt = st.session_state.messages + [{"role": "system", "content": status_context}]
+            with st.spinner("AI 코치가 생각 중...🤔"):
+                response = ai_client.chat.completions.create(
+                    model="gpt-5.4-mini",
+                    messages=prompt)
+                ai_response = response.choices[0].message.content
+                st.markdown(ai_response)
+        st.session_state.messages.append({"role": "assistant", "content": ai_response})
+        
+pg = st.navigation([
+    st.Page(page_motto, title="오늘의 다짐", icon="📣"),
+    st.Page(page_todo, title="오늘의 할 일", icon="✅"),
+    st.Page(page_report, title="나의 갓생 지수", icon="📈")], position="top")
 
+st.title("🌱 갓생 살기 플래너")
+pg.run()
