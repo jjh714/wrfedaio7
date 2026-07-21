68e04e4a87c2c79d4a2380fb0e26e4cccb0b544f
jjh714 <jeonjuheon1014@gmail.com>
2026-07-21 09:38:55 +0900

    Update 갓생플래너.py

--- 갓생플래너.py
+++ 갓생플래너.py
@@ -25,7 +25,7 @@ def page_todo():
     st.write(f"현재 다짐: **{st.session_state.user_motto}**")
     new_todo = st.text_input("추가할 할 일을 입력하세요", key="todo_input")
     st.button("추가하기", on_click=add_todo)
-if new_todo == "":
+    if new_todo == "":
        st.warning("할 일을 입력하고 버튼을 눌러주세요!")
 
 st.markdown("---")
