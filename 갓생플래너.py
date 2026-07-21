88a86a091db53e5797a48913d4a9018c70de1c32
jjh714 <jeonjuheon1014@gmail.com>
2026-07-21 09:38:33 +0900

    Fix indentation for motto saving button

--- 갓생플래너.py
+++ 갓생플래너.py
@@ -15,10 +15,10 @@ def add_todo():
 def page_motto():
     st.header("📣 1. 오늘의 다짐")
     motto = st.text_input("나의 한 줄 좌우명을 적어주세요")
-if st.button("다짐 저장"):
-   st.session_state.user_motto = motto
-   st.success("좌우명이 등록되었습니다!")
-   st.markdown("---")
+    if st.button("다짐 저장"):
+       st.session_state.user_motto = motto
+       st.success("좌우명이 등록되었습니다!")
+       st.markdown("---")
 
 def page_todo():
     st.header("✅ 2. 오늘의 할 일")
